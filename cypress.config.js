fbae670367165221a6abad99436bc9267ff4b762
Luis Perez <tito@Luiss-MacBook-Air.local>
2023-07-10 11:01:08 -0700

    updated test suite with new test cases

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  component: {
+    async devServer({ specs, cypressConfig, devServerEvents }) {
+      const { port, close } = await startDevServer(
+        specs,
+        cypressConfig,
+        devServerEvents
+      );
+
+      return {
+        port,
+        close,
+      };
+    },
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'ngx-cypress-test/cypress/e2e/**/*.*',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+  },
+});
