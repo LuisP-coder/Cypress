b349ee5b982d842ae45592ac7bfba352f29d3ebb
Luis Perez <tito@Luiss-MacBook-Air.local>
2023-11-06 10:46:10 -0800

    adding luma website test suite

---
+++ ngx-cypress-test/cypress/e2e/integrations/luma.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe('Automate dummy website', () => {
+    beforeEach(() => {
+        cy.visit('https://magento.softwaretestingboard.com/');
+    });
+
+    it(`Go to what's new page`, () => {
+        cy.get('.level-top')
+        .contains(`What's New`)
+        .click();
+
+        cy.contains(`What's New`);
+
+        cy.get('.column.main')
+        .should('be.visible');
+
+        cy.get('.block-promo.new-performance')
+        .children()
+        .should('have.attr', 'src', 'https://magento.softwaretestingboard.com/pub/media/wysiwyg/new/new-performance.jpg');
+
+        cy.get('.content')
+        .eq(3)
+        .children()
+        .contains('Whatever day brings');
+
+        cy.get('.block-promo.new-eco')
+        .children()
+        .should('have.attr', 'src', 'https://magento.softwaretestingboard.com/pub/media/wysiwyg/new/new-eco.jpg');
+
+        cy.get('.content')
+        .eq(4)
+        .children()
+        .contains('A sense of renewal');
+
+        cy.get('.content-heading')
+        .contains(`Luma's Latest`);
+
+        cy.get('.block.widget.block-products-list.grid')
+        .should('be.visible');
+
+        cy.get('.swatch-option.text')
+        .eq(1)
+        .click();
+    });
+});
