7c7470b90b8d34d1af1dd9cf0fc413cc27ef08a9
Luis Perez <68307398+LuisP-coder@users.noreply.github.com>
2024-04-16 12:34:35 -0700

    Update ultimate.v2.spec.js

--- ngx-cypress-test/cypress/e2e/integrations/ultimate.v2.spec.js
+++ ngx-cypress-test/cypress/e2e/integrations/ultimate.v2.spec.js
@@ -7,59 +7,64 @@ describe('Automate Ultimate QA Page Version 2', () => {
 
     it('Go to Java SDET Academy and verify', () => {
         // click on link
-        cy.get('.menu-item-type-custom')
-            .eq(1)
-            .click();
+       cy.get('ul[id="menu-main-menu"]')
+        .find('li[id="menu-item-218225"]')
+        .trigger('mouseover');
 
-        // Verify Url and title
-        cy.url().should('include', '/java-sdet');
-        cy.title().should('include', 'Java SDET Bootcamp')
+       
+        cy.get('.menu-item-object-custom.menu-item-217931')
+        .eq(1)
+        .click();
 
-        // Verify first section
-        cy.scrollTo(0, 250);
-        cy.get('.image__image')
-            .should('be.visible');
+        // // Verify Url and title
+        // cy.url().should('include', '/java-sdet');
+        // cy.title().should('include', 'Java SDET Bootcamp')
 
-        cy.get('h2')
-            .children()
-            .contains('Java Software Developers in Test (SDET)');
+        // // Verify first section
+        // cy.scrollTo(0, 250);
+        // cy.get('.image__image')
+        //     .should('be.visible');
 
-        cy.get('h5')
-            .children()
-            .find('span')
-            .should('have.css', 'font-weight', '400');
+        // cy.get('h2')
+        //     .children()
+        //     .contains('Java Software Developers in Test (SDET)');
 
-        cy.get('h5')
-            .find('span')
-            .filter(':contains("SDET")');
+        // cy.get('h5')
+        //     .children()
+        //     .find('span')
+        //     .should('have.css', 'font-weight', '400');
+
+        // cy.get('h5')
+        //     .find('span')
+        //     .filter(':contains("SDET")');
         
-        // verify second section
-        cy.get('.block')
-            .eq(2)
-            .should('be.visible');
+        // // verify second section
+        // cy.get('.block')
+        //     .eq(2)
+        //     .should('be.visible');
 
-        cy.get('#form_submission_name')
-            .eq(0)
-            .type('Tito');
+        // cy.get('#form_submission_name')
+        //     .eq(0)
+        //     .type('Tito');
         
-        cy.get('#form_submission_email')
-            .eq(0)
-            .type('testemail@automation.qa');
+        // cy.get('#form_submission_email')
+        //     .eq(0)
+        //     .type('testemail@automation.qa');
 
-        cy.get('#form_submission_address_country')
-            .eq(0)
-            .select(1);
+        // cy.get('#form_submission_address_country')
+        //     .eq(0)
+        //     .select(1);
         
-        cy.get('#form_submission_custom_2')
-            .eq(0)
-            .select(1);
+        // cy.get('#form_submission_custom_2')
+        //     .eq(0)
+        //     .select(1);
 
-        cy.get('#form_submission_custom_3')
-            .eq(0)
-            .select(3);
+        // cy.get('#form_submission_custom_3')
+        //     .eq(0)
+        //     .select(3);
 
-        cy.contains('Learn more!')
-            .eq(0)
-            .click();
+        // cy.contains('Learn more!')
+        //     .eq(0)
+        //     .click();
     });
 });
