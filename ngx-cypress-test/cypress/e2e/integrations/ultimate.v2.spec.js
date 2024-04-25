2d4873ec03e56b3ceb259dd5c293eb389c95130c
Luis Perez <68307398+LuisP-coder@users.noreply.github.com>
2024-04-25 08:03:34 -0700

    Update ultimate.v2.spec.js

--- ngx-cypress-test/cypress/e2e/integrations/ultimate.v2.spec.js
+++ ngx-cypress-test/cypress/e2e/integrations/ultimate.v2.spec.js
@@ -19,5 +19,22 @@ describe('Automate Ultimate QA Page Version 2', () => {
         
       cy.get('.et_pb_image_wrap img').eq(2).should('be.visible');
     });
+
+    it('Check 3 steps away section', () => {
+          // Check Title
+          cy.get('.et_pb_module_heading').eq(6).contains(`You're just 3 steps away from the UltimateQA testing solution`);
   
+          // Verify Boxes 
+          cy.get('.et_pb_image_wrap').eq(10).should('be.visible');
+          cy.get('.et_pb_blurb_description > p').eq(6).contains(`Book a Free Session to uncover tailored solutions for your QA challenges. Let’s explore together!`);
+  
+          cy.get('.et_pb_image_wrap').eq(11).should('be.visible');
+          cy.get('.et_pb_blurb_description > p').eq(7).contains(`Your Tailored Solution, Your Price – transparent, comprehensive, and crafted just for you. No surprises, just clarity.`);
+  
+          cy.get('.et_pb_image_wrap').eq(12).should('be.visible');
+          cy.get('.et_pb_blurb_description > p').eq(8).contains(`Implement and experience the transformation. Quality software to market faster – precision and speed redefined. Your success is our priority.`);
+      
+          // Verify button
+          cy.get('.et_pb_button_1').click();
+      });
 });
